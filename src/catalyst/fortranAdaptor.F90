fce6291d61bd047809d3d61ef51842cbb0ec8825
Jure Bartol <jure.bartol@lecad.fs.uni-lj.si>
2016-08-10 20:06:04 +0200

    Update fortranAdaptor.F90 and b2mndr.F to output more simulation data to ParaView Catalyst.

--- src/catalyst/fortranAdaptor.F90
+++ src/catalyst/fortranAdaptor.F90
@@ -1,32 +1,23 @@
 ! Fortran part of the adaptor for paraview catalyst for b2.5 simulation.
 ! Author: Jure Bartol
 ! Created on: 22.07.2016
-! Modified on: 03.08.2016
+! Modified on: 10.08.2016
 
 
-subroutine coprocessorstart()
-  character(len=200) :: arg
-  integer :: i, ilen
+subroutine coprocessor(crx,cry,ncrx,nx,ny,step,time,vol,hx,hy,qc,te,ti,po,qz,pbs,na,ua,uadia,fhe,fhi,fch,gs,bb)
+!Coprocessor is called each time step in b2mndr.F in order to output
+!simulation data to ParaView Catalyst.
 
-!initialize with python script as an argument (this probably won't be possible). Instead we will use
-! function coprocessorinitializewithpython("nameOfScript.py",<length of the file name>)
-  call coprocessorinitialize() 
-  do i=1, iargc()
-     call getarg(i, arg)
-     ilen = len_trim(arg)
-     arg(ilen+1:) = char(0)
-     call coprocessoraddpythonscript(arg, ilen)
-  enddo
-end subroutine
-
-
-subroutine coprocessor(crx,cry,ncrx,nx,ny,step,time)
-  use iso_c_binding ! IGNORE
   implicit none
-  integer :: nx, ny, step, flag
+  integer :: nx, ny, step, flag, numofcells
   integer, intent(in) :: ncrx
-  real :: time
-  real(kind=8), dimension(nx,ny,4) :: crx, cry
+  real(kind=8) :: time
+  real(kind=8), dimension(nx,ny,1) :: vol, hx, hy, qc, te, ti, po
+  real(kind=8), dimension(nx,ny,2) :: qz, pbs, na, ua, uadia, fhe, fhi, fch
+  real(kind=8), dimension(nx,ny,3) :: gs
+  real(kind=8), dimension(nx,ny,4) :: crx, cry, bb
+
+  numofcells=(nx+2)*(ny+2)
 
   call requestdatadescription(step,time,flag) 
   if (flag .ne. 0) then
@@ -34,8 +25,34 @@ subroutine coprocessor(crx,cry,ncrx,nx,ny,step,time)
      if (flag .ne. 0) then
         call creategrid(crx, cry, ncrx, nx, ny)
      end if
-     !call adddata(crx,"crx"//char(0),(nx+2)*(ny+2)) !//char(0) is a C++ terminating character in fortran
-     !call adddata(cry,"cry"//char(0),(nx+2)*(ny+2))
+!TODO variables:  fna(4d matrix)
+!1 component in each cell
+     call adddata(vol,"vol"//char(0),numofcells,1)
+     call adddata(hx,"hx"//char(0),numofcells,1)
+     call adddata(hy,"hy"//char(0),numofcells,1)
+     call adddata(qc,"qc"//char(0),numofcells,1)
+     call adddata(te,"te"//char(0),numofcells,1)
+     call adddata(ti,"ti"//char(0),numofcells,1)
+     call adddata(po,"po"//char(0),numofcells,1)
+!2 components in each cell
+     call adddata(qz,"qz"//char(0),numofcells,2)
+     call adddata(pbs,"pbs"//char(0),numofcells,2)
+     call adddata(na,"na"//char(0),numofcells,2)
+     call adddata(ua,"ua"//char(0),numofcells,2)
+     call adddata(uadia,"uadia"//char(0),numofcells,2)
+     call adddata(fhe,"fhe"//char(0),numofcells,2)
+     call adddata(fhi,"fhi"//char(0),numofcells,2)
+     call adddata(fch,"fch"//char(0),numofcells,2)
+!3 components in each cell
+     call adddata(gs,"gs"//char(0),numofcells,3) 
+!4 components in each cell
+     call adddata(crx,"crx"//char(0),numofcells,4)
+     call adddata(cry,"cry"//char(0),numofcells,4)
+!bb has 4 components, one of them is magnitude
+!which can be ignored, because paraview calculates it
+!on its own.
+     call adddata(bb,"bb"//char(0),numofcells,3)
+    
      call coprocess()
   end if
 end subroutine
