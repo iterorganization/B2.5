ceabd156f3ac506649bc2db4197befc61df7d583
Gregor Simič <gregor.simic@lecad.fss.uni-lj.si>
2017-03-10 18:07:14 +0100

    Add namelist atributes to modules. Solps-gui properly treats namelist and inserts switches in newline

--- src/documentation/b2cdcn.py
+++ src/documentation/b2cdcn.py
@@ -13,8 +13,8 @@ def get_default(text):
     base = ' Defaults to '
     end = '.'
     if text:
-        if '.true.' in text or '.false.' in text or 'NX' in text or 'NY' in \
-            text or 'NS' in text:
+        if '.true.' in text or '.false.' in 'nx' == text in 'ny' == text or \
+            text == 'ns' in text:
             return base + text + end
         if text.isalpha() or text == ' ' or text.replace('.', '').isalpha():
             return base + "'" + text + "'" + end
