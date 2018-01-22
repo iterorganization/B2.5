074f0e4872a232f456608412247e8e7ab202c167
Xavier Bonnin <Xavier.Bonnin@iter.org>
2018-01-22 09:55:03 +0100

    Enforcing a 4-digit limit for IDS run number

--- src/ids/b2_ual_write.F90
+++ src/ids/b2_ual_write.F90
@@ -86,6 +86,7 @@ program b2_ual_write
 
     call ipgeti( 'b2mndr_shot_number', shot )
     call ipgeti( 'b2mndr_run_number', run )
+    call xertst( 0.le.run.and.run.le.9999, 'Invalid run number')
 #ifdef NO_GETENV
     username=' '
 #else
