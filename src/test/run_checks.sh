a2a90e39d3dbb066a6ecde665429a3fb1612d666
Tamas Bela Feher <tamas.bela.feher@ipp.mpg.de>
2018-10-19 16:40:37 +0200

    Making the script work after the data are moved away from the b2mn.exe.dir, decreasing tolarence to 1e-11 for basic variables

--- src/test/run_checks.sh
+++ src/test/run_checks.sh
@@ -3,32 +3,44 @@ if [ ! -d "$1" ]; then
   echo "Error, first argument should be a directory!"
   exit
 fi
-if [ ! -d "$1/b2mn.exe.dir" ]; then
-  echo "Error, $1/b2mn.exe.dir not found!"
-  exit
-fi
 if [ ! -d "$2" ]; then
   echo "Error, second argument should be a directory!"
-  exit
-fi
-if [ ! -d "$2/b2mn.exe.dir" ]; then
-  echo "Error, $2/b2mn.exe.dir not found!"
   exit
 fi
 
 MYPATH=`dirname "$0"` # path to this script, used when we calling other scripts from the same directory
 
-for f in $1/b2mn.exe.dir/b2f*; do 
-   filenames="$f $2/b2mn.exe.dir/`basename $f`"; 
-   $MYPATH/check_b2_output $filenames  
+files="b2fmovie b2fparam b2fplasma b2fstate b2ftrace b2ftrack"
+
+#for f in $files; do 
+#   filenames="$1/$f $2/$f"; 
+#   $MYPATH/check_b2_output $filenames  
+#done  > compare_results.log
+
+missing=0
+for f in $files; do 
+   if [ ! -f $1/$f ]; then
+     echo "Error, file not found $1/$f";
+     missing=$((missing+1))
+   elif [ ! -f $2/$f ]; then
+     echo "ERROR, file not found $2/$f";
+     missing=$((missing+1))
+   else
+     $MYPATH/check_b2_output $1/$f $2/$f  
+   fi
 done  > compare_results.log
+
+if [ $missing -gt 0 ]; then
+  echo Results are NOT correct, files missing.
+  exit 1
+fi
 
 # For most of the variables we compare the maximum error to the average array value
 # (and this average is the average of abs(var)).
 # We have a large tolerance for most of the variables (1e-7).
 # For basic quantities like te, ti, na, ni, ne, po, ua we use more strick tolerance level.
 # Except for the velocity, the we check the maximum relative error for the basic quantities'
-$MYPATH/b2diff.py --tolerance 1e-7 --maxerr 'te ti na ni ne po' --specific-tolerance 'ua 1e-12 ne 1e-12 ni 1e-12 na 1e-10 te 1e-12 ti 1e-12 po 1e-12' -i 'del*|res*' -v compare_results.log
+$MYPATH/b2diff.py --tolerance 1e-7 --maxerr 'te ti na ni ne po' --specific-tolerance 'ua 1e-11 ne 1e-11 ni 1e-11 na 1e-10 te 1e-11 ti 1e-11 po 1e-11' -i 'del*|res*' -v compare_results.log
 
 STATUS=$? # exit status of b2diff.py
 # The exit status tells whether the test were successfull
