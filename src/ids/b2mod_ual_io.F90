c27416325c9fc6037af5d869d11f5be20bb15a17
Xavier Bonnin <xavier.bonnin@iter.org>
2019-06-20 15:33:11 +0200

    Compilation fixes for ITM environment
    Conflicts:
    	src/ids/b2mod_ual.F90

--- src/ids/b2mod_ual_io.F90
+++ src/ids/b2mod_ual_io.F90
@@ -50,6 +50,7 @@ module b2mod_ual_io
     use b2mod_ual_io_grid &
      & , only : findGridSubsetByName, GridWriteData, &
      &          b2_IMAS_Fill_Grid_Desc
+    use ids_routines       ! IGNORE
 #if IMAS_MINOR_VERSION > 14
     use ids_utility        ! IGNORE
 #endif
@@ -58,7 +59,7 @@ module b2mod_ual_io
         &   IDS_COORDTYPE_Z => COORDTYPE_Z,    &
         &   IDS_GRID_UNDEFINED => GRID_UNDEFINED
 #else
-#ifdef ITM
+#ifdef ITM_ENVIRONMENT_LOADED
     use euITM_schemas   ! IGNORE
     use euITM_routines  ! IGNORE
     use itm_grid_common ! IGNORE
@@ -1587,7 +1588,7 @@ contains
     end function unitVector
 
 #else
-# ifdef ITM
+# ifdef ITM_ENVIRONMENT_LOADED
 
   logical, parameter, private :: INCLUDE_GHOST_CELLS = .false.
 
