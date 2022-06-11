fde01115a1ebc8cad62be7f6fdf011ce1af8e469
Xavier Bonnin <Xavier.Bonnin@iter.org>
2022-06-11 08:34:36 +0200

    Updating size of Eirene GIT hash string where used

--- src/ids/b2mod_ual_io.F90
+++ src/ids/b2mod_ual_io.F90
@@ -6912,8 +6912,8 @@ contains
 #ifdef B25_EIRENE
     integer p
     character*8 eirene_version
-    character*31 Eirene_git_version
-    character*31 get_Eir_hash
+    character*32 Eirene_git_version
+    character*32 get_Eir_hash
 #endif
 #ifdef AMNS
     type (amns_handle_type) :: amns
