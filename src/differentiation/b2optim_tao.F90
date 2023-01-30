359fcd87557cc6f5772ae955024b304e46c23bf1
Stefano Carli <stefano.carli@kuleuven.be>
2023-01-30 10:51:41 +0100

    changes to compile b2optim_tao on ITER cluster

--- src/differentiation/b2optim_tao.F90
+++ src/differentiation/b2optim_tao.F90
@@ -57,46 +57,67 @@
         write (*,*) 'PETSC-TAO, WARNING: setting Hessian as identity matrix for Steepest Descent method'
       endif
 
-      call InitializeProblem(npar_opt,ierr);CHKERRA(ierr)
+      call InitializeProblem(npar_opt,ierr)
+      CHKERRA(ierr)
 
-      call TaoCreate(PETSC_COMM_SELF,tao,ierr);CHKERRA(ierr)
+      call TaoCreate(PETSC_COMM_SELF,tao,ierr)
+      CHKERRA(ierr)
 !     Here better coding can be done to have different methods?
       if (hessian) then
-        call TaoSetType(tao,TAOTRON,ierr);CHKERRA(ierr) 
+        call TaoSetType(tao,TAOTRON,ierr)
+        CHKERRA(ierr)
       else
-        call TaoSetType(tao,TAOBQNLS,ierr);CHKERRA(ierr) 
+        call TaoSetType(tao,TAOBQNLS,ierr)
+        CHKERRA(ierr)
       endif
-      call TaoSetInitialVector(tao,X,ierr);CHKERRA(ierr)
-      call TaoSetVariableBounds(tao,X_L,X_U,ierr);CHKERRA(ierr) 
-      call TaoSetObjectiveAndGradientRoutine(tao,FormFunctionGradient,0,ierr);CHKERRA(ierr) 
-      call TaoSetObjectiveRoutine(tao,FormFunction,0,ierr);CHKERRA(ierr) 
-      call TaoSetGradientRoutine(tao,FormGradient,0,ierr);CHKERRA(ierr) 
+      call TaoSetInitialVector(tao,X,ierr)
+      CHKERRA(ierr)
+      call TaoSetVariableBounds(tao,X_L,X_U,ierr)
+      CHKERRA(ierr)
+      call TaoSetObjectiveAndGradientRoutine(tao,FormFunctionGradient,0,ierr)
+      CHKERRA(ierr)
+      call TaoSetObjectiveRoutine(tao,FormFunction,0,ierr)
+      CHKERRA(ierr)
+      call TaoSetGradientRoutine(tao,FormGradient,0,ierr)
+      CHKERRA(ierr)
       if (hessian) then
-        call TaoSetHessianRoutine(tao,Hess,Hess,FormHessian,0,ierr);CHKERRA(ierr)
+        call TaoSetHessianRoutine(tao,Hess,Hess,FormHessian,0,ierr)
+        CHKERRA(ierr)
       endif
 
-      call TaoSetFromOptions(tao,ierr);CHKERRA(ierr) 
+      call TaoSetFromOptions(tao,ierr)
+      CHKERRA(ierr)
 
       ! Set a minimum tolerance
       call xertst (tol_opt.gt.0.0, 'faulty internal parameter tol_opt')
-      call TaoSetTolerances(tao,tol_opt,tol_opt,tol_opt,ierr);CHKERRA(ierr)
+      call TaoSetTolerances(tao,tol_opt,tol_opt,tol_opt,ierr)
+      CHKERRA(ierr)
 
       ! Set max number of iterations
       call xertst (maxiter.gt.0, 'faulty internal parameter maxiter')
-      call TaoSetMaximumIterations(tao, maxiter, ierr);CHKERRA(ierr)
-      call TaoSetMaximumFunctionEvaluations(tao, 2*maxiter, ierr);CHKERRA(ierr)
+      call TaoSetMaximumIterations(tao, maxiter, ierr)
+      CHKERRA(ierr)
+      call TaoSetMaximumFunctionEvaluations(tao, 2*maxiter, ierr)
+      CHKERRA(ierr)
 
       ! Solve
-      call TaoSolve(tao,ierr);CHKERRA(ierr) 
-      call PetscViewerASCIIOpen(PETSC_COMM_WORLD,'PETSC-TAO.OUT',viewer,ierr);CHKERRA(ierr)
-      call PetscViewerPushFormat(viewer,PETSC_VIEWER_ASCII_INFO_DETAIL,ierr);CHKERRA(ierr)
-      call TaoView(tao,viewer,ierr);CHKERRA(ierr)
-      call PetscViewerDestroy(viewer,ierr);CHKERRA(ierr)
+      call TaoSolve(tao,ierr)
+      CHKERRA(ierr)
+      call PetscViewerASCIIOpen(PETSC_COMM_WORLD,'PETSC-TAO.OUT',viewer,ierr)
+      CHKERRA(ierr)
+      call PetscViewerPushFormat(viewer,PETSC_VIEWER_ASCII_INFO_DETAIL,ierr)
+      CHKERRA(ierr)
+      call TaoView(tao,viewer,ierr)
+      CHKERRA(ierr)
+      call PetscViewerDestroy(viewer,ierr)
+      CHKERRA(ierr)
 
       ! Finalize Memory
-      call DestroyProblem(ierr);CHKERRA(ierr)
+      call DestroyProblem(ierr)
+      CHKERRA(ierr)
 
-      call TaoDestroy(tao,ierr);CHKERRA(ierr)
+      call TaoDestroy(tao,ierr)
+      CHKERRA(ierr)
       call PetscFinalize(ierr)
 
       call b2mn_fin_diff(switch, geo, geodiff, mpg, mpgdiff, state, statediff,&
@@ -121,41 +142,59 @@
 
       zero = 0.0
 
-      call VecCreateSeq(PETSC_COMM_SELF,npar,X,ierr);CHKERRQ(ierr)
-      call VecDuplicate(X,X_L,ierr);CHKERRQ(ierr)
-      call VecDuplicate(X,X_U,ierr);CHKERRQ(ierr)
-      call VecSet(X,zero,ierr);CHKERRQ(ierr)
-      call VecSet(X_L,zero,ierr);CHKERRQ(ierr)
-      call VecSet(X_U,zero,ierr);CHKERRQ(ierr)
+      call VecCreateSeq(PETSC_COMM_SELF,npar,X,ierr)
+      CHKERRQ(ierr)
+      call VecDuplicate(X,X_L,ierr)
+      CHKERRQ(ierr)
+      call VecDuplicate(X,X_U,ierr)
+      CHKERRQ(ierr)
+      call VecSet(X,zero,ierr)
+      CHKERRQ(ierr)
+      call VecSet(X_L,zero,ierr)
+      CHKERRQ(ierr)
+      call VecSet(X_U,zero,ierr)
+      CHKERRQ(ierr)
 
       do ipar = 1, npar
-        call VecSetValue(X,ipar-1,x0(ipar)/par_rescale(ipar),INSERT_VALUES,ierr);CHKERRQ(ierr)
+        call VecSetValue(X,ipar-1,x0(ipar)/par_rescale(ipar),INSERT_VALUES,ierr)
+        CHKERRQ(ierr)
         if (xl(ipar).lt.-inf_opt) then
           write(*,*) 'TAO: warning, X_L(',ipar,') set to infty'
-          call VecSetValue(X_L,ipar-1,PETSC_NINFINITY,INSERT_VALUES,ierr);CHKERRQ(ierr)
+          call VecSetValue(X_L,ipar-1,PETSC_NINFINITY,INSERT_VALUES,ierr)
+          CHKERRQ(ierr)
         else
-          call VecSetValue(X_L,ipar-1,xl(ipar)/par_rescale(ipar),INSERT_VALUES,ierr);CHKERRQ(ierr)
+          call VecSetValue(X_L,ipar-1,xl(ipar)/par_rescale(ipar),INSERT_VALUES,ierr)
+          CHKERRQ(ierr)
         endif
         if (xu(ipar).gt.inf_opt) then
           write(*,*) 'TAO: warning, X_U(',ipar,') set to infty'
-          call VecSetValue(X_U,ipar-1,PETSC_INFINITY,INSERT_VALUES,ierr);CHKERRQ(ierr)
+          call VecSetValue(X_U,ipar-1,PETSC_INFINITY,INSERT_VALUES,ierr)
+          CHKERRQ(ierr)
         else
-          call VecSetValue(X_U,ipar-1,xu(ipar)/par_rescale(ipar),INSERT_VALUES,ierr);CHKERRQ(ierr)
+          call VecSetValue(X_U,ipar-1,xu(ipar)/par_rescale(ipar),INSERT_VALUES,ierr)
+          CHKERRQ(ierr)
         endif
       end do
-      call VecAssemblyBegin(x,ierr);CHKERRQ(ierr)
-      call VecAssemblyEnd(x,ierr);CHKERRQ(ierr)
+      call VecAssemblyBegin(x,ierr)
+      CHKERRQ(ierr)
+      call VecAssemblyEnd(x,ierr)
+      CHKERRQ(ierr)
       write(*,*) 'TAO SET X0'
-      call VecView(X,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+      call VecView(X,PETSC_VIEWER_STDOUT_WORLD,ierr)
+      CHKERRQ(ierr)
       write(*,*) 'TAO SET X_L'
-      call VecView(X_L,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+      call VecView(X_L,PETSC_VIEWER_STDOUT_WORLD,ierr)
+      CHKERRQ(ierr)
       write(*,*) 'TAO SET X_U'
-      call VecView(X_U,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+      call VecView(X_U,PETSC_VIEWER_STDOUT_WORLD,ierr)
+      CHKERRQ(ierr)
       ierr = 0
 
 !  Allocate storage space for Hessian;
-      call MatCreateSeqBAIJ(PETSC_COMM_SELF,npar,npar,npar,1,PETSC_NULL_INTEGER,Hess,ierr);CHKERRQ(ierr)
-      call MatSetOption(Hess,MAT_SYMMETRIC,PETSC_TRUE,ierr);CHKERRQ(ierr)
+      call MatCreateSeqBAIJ(PETSC_COMM_SELF,npar,npar,npar,1,PETSC_NULL_INTEGER,Hess,ierr)
+      CHKERRQ(ierr)
+      call MatSetOption(Hess,MAT_SYMMETRIC,PETSC_TRUE,ierr)
+      CHKERRQ(ierr)
 
       end subroutine InitializeProblem
 
@@ -166,10 +205,14 @@
 
       PetscErrorCode ierr
 
-      call VecDestroy(X,ierr);CHKERRQ(ierr)
-      call VecDestroy(X_L,ierr);CHKERRQ(ierr)
-      call VecDestroy(X_U,ierr);CHKERRQ(ierr)
-      call MatDestroy(Hess,ierr);CHKERRQ(ierr)
+      call VecDestroy(X,ierr)
+      CHKERRQ(ierr)
+      call VecDestroy(X_L,ierr)
+      CHKERRQ(ierr)
+      call VecDestroy(X_U,ierr)
+      CHKERRQ(ierr)
+      call MatDestroy(Hess,ierr)
+      CHKERRQ(ierr)
       ierr = 0
       end subroutine DestroyProblem
 
@@ -188,7 +231,7 @@
       integer ipar, isigma, idum(0:2)
       integer, save :: iter = 0
       character*3 str
-      character(22) :: opt_state_name 
+      character(22) :: opt_state_name
       character*120 label
       PetscErrorCode ierr
       PetscInt dummy
@@ -197,8 +240,10 @@
       PetscScalar F
       PetscReal, pointer :: x_v(:), g_v(:)
 
-      call VecGetArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
-      call VecGetArrayF90(grad,g_v,ierr);CHKERRQ(ierr)
+      call VecGetArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
+      call VecGetArrayF90(grad,g_v,ierr)
+      CHKERRQ(ierr)
 
       do ipar = 1, npar_opt - nsigma_opt
         par_opt_phys(ipar) = x_v(ipar)*par_rescale(ipar)
@@ -236,12 +281,14 @@
       end do
 #endif
       write (*,*) 'TAO GRADIENT NORM:', norm2(g_v(1:npar_opt))
-      call VecRestoreArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
-      call VecRestoreArrayF90(grad,g_v,ierr);CHKERRQ(ierr)
+      call VecRestoreArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
+      call VecRestoreArrayF90(grad,g_v,ierr)
+      CHKERRQ(ierr)
 ! Experimental: write intermediate state file?
       if (iter .gt. 0) then
         write(*,*) 'Saving intermediate optimization state'
-        write (opt_state_name,'(a14,i4.4)') 'b2fstate.',iter
+        write (opt_state_name,'(a14,i4.4)') 'b2fstate_optim.',iter
         call cfopen (99,trim(opt_state_name),'new','un*formatted')
         idum(0) = mpg%nCv
         idum(1) = mpg%nFc
@@ -272,7 +319,8 @@
       PetscScalar F
       PetscReal, pointer :: x_v(:)
 
-      call VecGetArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
+      call VecGetArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
 
       do ipar = 1, npar_opt - nsigma_opt
         par_opt_phys(ipar) = x_v(ipar)*par_rescale(ipar)
@@ -293,7 +341,8 @@
       call b2mn_step(switch, geo, mpg, state, state_ext, j)
       F = j(1)
 
-      call VecRestoreArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
+      call VecRestoreArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
       ierr = 0
       end subroutine FormFunction
 
@@ -310,7 +359,7 @@
       integer ipar, isigma, idum(0:2)
       character*3 str
       integer, save :: iter = 0
-      character(22) :: opt_state_name 
+      character(22) :: opt_state_name
       character*120 label
       PetscErrorCode ierr
       PetscInt dummy
@@ -319,8 +368,10 @@
       PetscReal, pointer :: x_v(:), g_v(:)
 
 
-      call VecGetArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
-      call VecGetArrayF90(grad,g_v,ierr);CHKERRQ(ierr)
+      call VecGetArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
+      call VecGetArrayF90(grad,g_v,ierr)
+      CHKERRQ(ierr)
 
       do ipar = 1, npar_opt - nsigma_opt
         par_opt_phys(ipar) = x_v(ipar)*par_rescale(ipar)
@@ -360,7 +411,7 @@
 ! Experimental: write intermediate state file?
       if (iter .gt. 0) then
         write(*,*) 'Saving intermediate optimization state'
-        write (opt_state_name,'(a14,i4.4)') 'b2fstate..',iter
+        write (opt_state_name,'(a14,i4.4)') 'b2fstate_optim..',iter
         call cfopen (99,trim(opt_state_name),'new','un*formatted')
         idum(0) = mpg%nCv
         idum(1) = mpg%nFc
@@ -374,8 +425,10 @@
         close(99)
       endif
       iter = iter + 1
-      call VecRestoreArrayReadF90(XX,x_v,ierr);CHKERRQ(ierr)
-      call VecRestoreArrayF90(grad,g_v,ierr);CHKERRQ(ierr)
+      call VecRestoreArrayReadF90(XX,x_v,ierr)
+      CHKERRQ(ierr)
+      call VecRestoreArrayF90(grad,g_v,ierr)
+      CHKERRQ(ierr)
       ierr = 0
       end subroutine FormGradient
 
@@ -393,20 +446,26 @@
       PetscReal        v
 
  !  Zero existing matrix entries
-      call MatAssembled(HH,assembled,ierr);CHKERRQ(ierr)
-      if (assembled .eqv. PETSC_TRUE) call MatZeroEntries(HH,ierr);CHKERRQ(ierr)
+      call MatAssembled(HH,assembled,ierr)
+      CHKERRQ(ierr)
+      if (assembled .eqv. PETSC_TRUE) call MatZeroEntries(HH,ierr)
+      CHKERRQ(ierr)
 
  !  Compute Hessian entries
       do i=0,npar_opt-1
         v = 1.0
-        call MatSetValues(HH,1,i,1,i,v,INSERT_VALUES,ierr);CHKERRQ(ierr)
+        call MatSetValues(HH,1,i,1,i,v,INSERT_VALUES,ierr)
+        CHKERRQ(ierr)
       enddo
 
  !  Assemble matrix
-      call MatAssemblyBegin(HH,MAT_FINAL_ASSEMBLY,ierr);CHKERRQ(ierr)
-      call MatAssemblyEnd(HH,MAT_FINAL_ASSEMBLY,ierr);CHKERRQ(ierr)
+      call MatAssemblyBegin(HH,MAT_FINAL_ASSEMBLY,ierr)
+      CHKERRQ(ierr)
+      call MatAssemblyEnd(HH,MAT_FINAL_ASSEMBLY,ierr)
+      CHKERRQ(ierr)
       write(*,*) 'TAO SET H'
-      call MatView(Hess,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+      call MatView(Hess,PETSC_VIEWER_STDOUT_WORLD,ierr)
+      CHKERRQ(ierr)
 
       return
       end subroutine FormHessian
