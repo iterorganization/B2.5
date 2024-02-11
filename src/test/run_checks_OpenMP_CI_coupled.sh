cbf93c150a3089b6444e196ef6888df48c61210e
Xavier Bonnin <Xavier.Bonnin@iter.org>
2024-02-11 13:22:38 +0100

    More relaxing of criteria for OpenMP CI coupled test case

--- src/test/run_checks_OpenMP_CI_coupled.sh
+++ src/test/run_checks_OpenMP_CI_coupled.sh
@@ -37,7 +37,7 @@ fi
 # For each stratum, the NPTSDEL number must be equal to the NPTS value divided by some
 # integer multiple of the number of threads used.
 # Except for the velocity, we check the maximum relative error for the basic quantities.
-$MYPATH/b2diff.py --tolerance 0.01 --maxerr 'te ti na ni ne po' --specific-tolerance 'fhe 0.1 fhe_eir 0.02 fhep 0.10 fhe0 0.05 fhe_mdf 0.1 fhi 0.05 fhi0 0.05 fhi_eir 0.05 fhet 0.25 fhit 0.05 fhi_mdf 0.05 fhip 0.05 fht 0.05 fhm 0.15 fna0 0.10 fmo 0.10 fne_eir 0.1 te 0.1 ti 0.06 ti0 0.02 tip 0.02 po 0.1 pop 0.1 ne2 0.02 calf 0.10 chce 0.25 hce0 0.10 chci 0.10 hci0 0.10 fne_32 0.1 fne_52 0.1 fch_32 0.02 fch_52 0.025' -i 'time|data|b2stb*|res*|del*|sm*|po0|na*|ne0|nep|ni0|ua*|kinrgy|fna_he|fna_52|fch|fch0|fchp|fch_p|fhj|fne|fni*|rcx*|rqahe|rqrad|rra*|rsa*|alf*|calf_an|cdpa|csig*|dpa*|fllim*|b2sihs_*|b2npmo_sm*' -v compare_results.log
+$MYPATH/b2diff.py --tolerance 0.01 --maxerr 'te ti na ni ne po' --specific-tolerance 'fhe 0.1 fhe_eir 0.02 fhep 0.10 fhe0 0.05 fhe_mdf 0.1 fhi 0.05 fhi0 0.05 fhi_eir 0.05 fhet 0.25 fhit 0.05 fhi_mdf 0.05 fhip 0.05 fht 0.05 fhm 0.15 fna0 0.10 fmo 0.10 fne_eir 0.1 te 0.1 ti 0.1 ti0 0.1 tip 0.1 po 0.25 pop 0.25 ne2 0.02 calf 0.10 chce 0.25 hce0 0.10 chci 0.10 hci0 0.10 fne_32 0.1 fne_52 0.1 fch_32 0.02 fch_52 0.025' -i 'time|data|b2stb*|res*|del*|sm*|po0|na*|ne0|nep|ni0|ua*|kinrgy|fna_he|fna_52|fch|fch0|fchp|fch_p|fhj|fne|fni*|rcx*|rqahe|rqrad|rra*|rsa*|alf*|calf_an|cdpa|csig*|dpa*|fllim*|b2sihs_*|b2npmo_sm*' -v compare_results.log
 
 STATUS=$? # exit status of b2diff.py
 # The exit status tells whether the test were successful
