cb4bc6c2a47e3d9a3f69309057440e5eda826a80
Sergei Makarov <makaros>
2024-05-09 21:10:19 +0200

    Rename the condutivity varible according to the new notation

--- src/ids/b2mod_ual_io.F90
+++ src/ids/b2mod_ual_io.F90
@@ -872,7 +872,7 @@ contains
             &        chvi, csig, csigin, calf, cthe, cthi,                        &
             &        cdnahz, cdpahz, cvlahz, cvmahz, cvsahz, cvsa_cl, cvsahz_cl,  &
             &        cvsahz_drho, cvsa_drho,                                      & !som 18.08.21
-            &        chci_al, chci_a, cvsahz_hAdp_albe, cvsahz_hBdp_al,           & !som 08.10.21
+            &        chci_al_ast, chci_a, cvsahz_hAdp_albe, cvsahz_hBdp_al,       & !som 08.10.21
             &        cvsa_hAdp_albe, cvsa_hBdp_al,                                & !som 20.10.21
             &        cvsahz_uAdp_albe, cvsahz_uBdp_al,                            & !som 22.12.23
             &        cvsa_uAdp_albe, cvsa_uBdp_al,                                & !som 26.04.24
@@ -897,7 +897,7 @@ contains
             &        fchdia, fchin, fch_p, fchvispar, fchvisper, fchvisq,        &
             &        fchinert, fchanml, fna_eir, fne_eir, fhe_eir, fhi_eir,      &
             &        cdna, cdpa, cvsa_cl, cvsa_drho,                             & !som 18.08.21
-            &        chci_al, chci_a, cvsa_hAdp_albe, cvsa_hBdp_al,              & !som 20.10.21
+            &        chci_al_ast, chci_a, cvsa_hAdp_albe, cvsa_hBdp_al,          & !som 20.10.21
             &        cvsa_uAdp_albe, cvsa_uBdp_al,                               & !som 26.04.24
             &        cvla, chce, chve, chci, chvi, calf,                         &
             &        rlsa, rlra, rlqa, rlcx, rlrd, rlbr,                         &
