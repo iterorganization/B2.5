ba5f77808766f471f06d73d3af8004a443eb3aa3
Xavier Bonnin <Xavier.Bonnin@iter.org>
2024-08-29 10:31:14 +0200

    Adaptations to IMAS DD/3.42.0

--- src/ids/b2mod_ual_io.F90
+++ src/ids/b2mod_ual_io.F90
@@ -7775,8 +7775,14 @@ contains
     properties%comment = comment
 #if ( IMAS_MINOR_VERSION > 33 || IMAS_MAJOR_VERSION > 3 )
     allocate( properties%provenance%node(1) )
+#if ( IMAS_MINOR_VERSION > 41 || IMAS_MAJOR_VERSION > 3 )
+    allocate( properties%provenance%node(1)%reference(1) )
+    allocate( properties%provenance%node(1)%reference(1)%name(1) )
+    properties%provenance%node(1)%reference(1)%name(1) = source
+#else
     allocate( properties%provenance%node(1)%sources(1) )
     properties%provenance%node(1)%sources(1) = source
+#endif
 #else
     allocate( properties%source(1) )
     properties%source = source
@@ -8286,12 +8292,21 @@ contains
             allocate( equilibrium%ids_properties%provenance%node(inode + 1) )
             allocate( &
                & equilibrium%ids_properties%provenance%node(inode+1)%path(1) )
-            allocate( &
-               & equilibrium%ids_properties%provenance%node(inode+1)%sources(1))
             equilibrium%ids_properties%provenance%node(inode+1)%path =        &
                & "grids_ggd"
+#if ( IMAS_MINOR_VERSION > 41 || IMAS_MAJOR_VERSION > 3 )
+            allocate( &
+               & equilibrium%ids_properties%provenance%node(inode+1)%reference(1) )
+            allocate( &
+               & equilibrium%ids_properties%provenance%node(inode+1)%reference(1)%name(1) )
+            equilibrium%ids_properties%provenance%node(inode+1)%reference(1)%name(1) =  &
+               &  source
+#else
+            allocate( &
+               & equilibrium%ids_properties%provenance%node(inode+1)%sources(1) )
             equilibrium%ids_properties%provenance%node(inode+1)%sources(1) =  &
                &  source
+#endif
 #endif
           end if
           if (.not.associated( equilibrium%time_slice )) then
